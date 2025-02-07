568ce3ac9dd0335a4deb5e89dc9b93ded114951d
ETMORTY <ethanm8@byu.edu>
2025-02-06 22:45:52 -0700

    Fixing stories

--- src/stories/stories.jsx
+++ src/stories/stories.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './stories.css';
 import { Link } from 'react-router-dom';
 
+const myStyle = {}
+
 export function Stories() {
   return (
     <main className="container-fluid">
@@ -14,77 +16,77 @@ export function Stories() {
             <p><em>A catalog of stories to select from</em></p>
         </div>
         <div className="stories-container">
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 1</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <Link to="/ReadStory" className="btn btn-primary" id="Story1">Begin date!</Link>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 2</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story2">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 3</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story3">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 4</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story4">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 5</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story5">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 6</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story6">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 7</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story7">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 8</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story8">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 9</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story9">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 10</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
                     <a href="ReadStory.html" className="btn btn-primary" id="Story10">Begin date!</a>
                 </div>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Story 11</h5>
                     <p className="card-text">This is a story that has been submitted by a user. It will be a choose your own adventure story that will be created as you go.</p>
@@ -94,7 +96,7 @@ export function Stories() {
         </div>
         <div className="AddButton">
             <label for="button"><em>Add your own:</em></label>
-            <a href="SubmitStory.html" className="btn btn-info">+</a>
+            <Link to='/SubmitStory' className="btn btn-info">+</Link>
         </div>
     </main>
   );
