e1dac181704d565f9cb98493524ad97276fd9e03
ETMORTY <ethanm8@byu.edu>
2025-02-08 21:51:09 -0700

    Fixed login styling and buttons

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './login.css';
+import { Link } from 'react-router-dom';
 
 export function Login() {
     return (
         <main className="container-fluid text-center">
-                <div>
+                <div className="content">
                         <h1>Login</h1>
-                        <form action="stories.html" method="get" className="login">
+                        <form action="/stories" method="get" className="login">
                                 <div className="input-group mb-3">
                                         <span className="input-group-text">Email: </span>
                                         <input type="email" className="form-control" placeholder="your@email.com" />
@@ -14,8 +16,10 @@ export function Login() {
                                         <span className="input-group-text">Password: </span>
                                         <input type="password" className="form-control" placeholder="Password" />
                                 </div>
-                                <input type="submit" value="Login" className="btn btn-primary" />
-                                <input type="submit" value="Create Account" className="btn btn-secondary" />
+                                {/* <input type="submit" value="Login" className="btn btn-primary" />
+                                <input type="submit" value="Create Account" className="btn btn-secondary" /> */}
+                                <Link to="/stories" className="btn btn-primary">Login</Link>
+                                <Link to="/stories" className="btn btn-secondary">Create Account</Link>
                         </form>
                 </div>
         </main>
