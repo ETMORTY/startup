6d38c41a31c7d1fcf6bfdee645f8f563be9f4c4d
ETMORTY <ethanm8@byu.edu>
2025-02-19 22:47:39 -0700

    Fixing class framework

--- src/SubmitStory/story.js
+++ src/SubmitStory/story.js
@@ -7,21 +7,22 @@ class Option {
 }
 
 class Story {
-    constructor() {
-        new Option(Intro, 1A, 1B);
-        new Option(1A, 2A, 2B);
-        new Option(1B, 2C, 2D);
-        new Option(2A, 3A, 3B);
-        new Option(2B, 3C, 3D);
-        new Option(2C, 3E, 3F);
-        new Option(2D, 3G, 3H);
-        new Option(3A, 4A, 4B);
-        new Option(3B, 4C, 4D);
-        new Option(3C, 4E, 4F);
-        new Option(3D, 4G, 4H);
-        new Option(3E, 4I, 4J);
-        new Option(3F, 4K, 4L);
-        new Option(3G, 4M, 4N);
-        new Option(3H, 4O, 4P);
+    constructor(Title, Intro, oneA, oneB, twoA, twoB, twoC, twoD, threeA, threeB, threeC, threeD, threeE, threeF, threeG, threeH, fourA, fourB, fourC, fourD, fourE, fourF, fourG, fourH, fourI, fourJ, fourK, fourL, fourM, fourN, fourO, fourP) {
+        this.Title = Title;
+        new Option(Intro, oneA, oneB);
+        new Option(oneA, twoA, twoB);
+        new Option(oneB, twoC, twoD);
+        new Option(twoA, threeA, threeB);
+        new Option(twoB, threeC, threeD);
+        new Option(twoC, threeE, threeF);
+        new Option(twoD, threeG, threeH);
+        new Option(threeA, fourA, fourB);
+        new Option(threeB, fourC, fourD);
+        new Option(threeC, fourE, fourF);
+        new Option(threeD, fourG, fourH);
+        new Option(threeE, fourI, fourJ);
+        new Option(threeF, fourK, fourL);
+        new Option(threeG, fourM, fourN);
+        new Option(threeH, fourO, fourP);
     }
 }
